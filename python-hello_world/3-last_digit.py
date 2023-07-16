359c5e8267dec6646df6c7603bc3141da07d7e2e
Stephen Tawiah <stawiah17@gmail.com>
2023-07-16 10:17:05 -0700

    fixing errors

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
 s_num = "Last digit of "
-greater = " and is greater than "
+greater = "and is greater than 5"
 negative = " and is less than 6 and not "
 if (last > 5):
-    print(s_num + "{0} is {1}" + greater + "5".format(str(number), str(last)))
+    print(f"{last} {number} is {last} {greater}")
 elif (last == 0):
     print(s_num + "{} is {} and is 0".format(str(number), str(last)))
 elif (last < 6 and last != 0):
