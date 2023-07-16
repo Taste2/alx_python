2819d22de87e2e6548d500a287ce3d30a303c9e4
Stephen Tawiah <stawiah17@gmail.com>
2023-07-16 09:35:09 -0700

    checked spacing and formating

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-s_num = "Last digit of"
-if (last_digit > 5):
-    print(s_num + "{} is {} and is greater than 5".format(str(number), str(last_digit)))
-elif (last_digit == 0):
-    print(s_num + "{} is {} and is 0".format(str(number), str(last_digit)))
-elif (last_digit < 6 and last_digit != 0):
-    print(s_num + "{} is {} and is less than 6 and not 0")
+last = number % 10
+s_num = "Last digit of "
+if (last > 5):
+    print(s_num +"{} is {} and is greater than 5".format(str(number), str(last)))
+elif (last == 0):
+    print(s_num +"{} is {} and is 0".format(str(number), str(last)))
+elif (last < 6 and last != 0):
+    print(s_num +"{} is {} and is less than 6 and not 0".format(str(number), str(last)))
