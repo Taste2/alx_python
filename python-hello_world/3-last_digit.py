1cb5ebe0357b5a0bd072144230c6f8e7db0198f7
Stephen Tawiah <stawiah17@gmail.com>
2023-07-16 09:26:28 -0700

    reduce lines

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,11 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
+s_num = "Last digit of"
 if (last_digit > 5):
-    print("Last digit of {} is {} and is greater than 5
-            ".format(str(number), str(last_digit)))
+    print(s_num + "{} is {} and is greater than 5".format(str(number), str(last_digit)))
 elif (last_digit == 0):
-    print("Last digit of {} is {} and is 0"
-            .format(str(number), str(last_digit)))
+    print(s_num + "{} is {} and is 0".format(str(number), str(last_digit)))
 elif (last_digit < 6 and last_digit != 0):
-    print("Last digit of {} is {} and not 0")
+    print(s_num + "{} is {} and is less than 6 and not 0")
