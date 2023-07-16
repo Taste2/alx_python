8d78b93d5ecac179f7a166bb6e533507a004858b
Stephen Tawiah <stawiah17@gmail.com>
2023-07-16 09:47:15 -0700

    typo checked

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 last = number % 10
 s_num = "Last digit of "
-five_ = " and is gretaer than 5"
+fi_ve = " and is greater than 5"
 six_zero = " and is less than 6 and not 0"
 if (last > 5):
-    print(s_num + "{} is {}" + five_.format(str(number), str(last)))
+    print(s_num + "{} is {}" + fi_ve.format(str(number), str(last)))
 elif (last == 0):
     print(s_num + "{} is {} and is 0".format(str(number), str(last)))
 elif (last < 6 and last != 0):
