b892e729d69c7dc78c45cbd039463f5e855c7480
Stephen Tawiah <stawiah17@gmail.com>
2023-07-16 10:10:37 -0700

    fixing negative and format

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,8 +6,8 @@ s_num = "Last digit of "
 greater = " and is greater than "
 negative = " and is less than 6 and not "
 if (last > 5):
-    print(s_num + "{} is {}" + greater + "5".format(str(number), str(last)))
+    print(s_num + "{0} is {1}" + greater + "5".format(str(number), str(last)))
 elif (last == 0):
     print(s_num + "{} is {} and is 0".format(str(number), str(last)))
 elif (last < 6 and last != 0):
-    print(s_num + "{} is -{}" + negative + "0".format(str(number), str(last)))
+    print(s_num + "{0} is -{1}" + negative + "0".format(str(number), str(last)))
