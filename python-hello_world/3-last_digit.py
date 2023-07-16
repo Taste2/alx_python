9c02312b5c973ed25d42a5e97b56abcbd6027124
Stephen Tawiah <stawiah17@gmail.com>
2023-07-16 09:03:22 -0700

    task 3

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+if (last_digit > 5):
+    print("Last digit of {} is {} and is greater than 5".format(str(number), str(last_digit)))
+elif (last_digit == 0):
+    print("Last digit of {} is {} and is zero".format(str(number), str(last_digit)))
+elif (last_digit < 6 and != 0):
+    print("Last digit of {} is {} and not 0")
