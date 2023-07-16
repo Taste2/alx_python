ba4cb9c8950fb4da45e84e14dca5380b7cb42a86
Stephen Tawiah <stawiah17@gmail.com>
2023-07-16 08:20:27 -0700

    made exe

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-print('\"Programming is like building a multilingual puzzle')
-=======
-print('\"Programming is like building a multilingual puzzle\n')
->>>>>>> 429f823432d8c5e995a1c357437c4b59e5413e90
+print('"Programming is like building a multilingual puzzle')
