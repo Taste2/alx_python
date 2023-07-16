1356d7680d2a908e411783dff663aee526e7104a
Stephen Tawiah <stawiah17@gmail.com>
2023-07-16 10:06:45 -0700

    fixing negative value

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+last = abs(number) % 10
 s_num = "Last digit of "
-greater = " and is greater than 5"
-six_zero = " and is less than 6 and not 0"
+greater = " and is greater than "
+negative = " and is less than 6 and not "
 if (last > 5):
-    print(s_num + "{} is {}" + greater.format(str(number), str(last)))
+    print(s_num + "{} is {}" + greater + "5".format(str(number), str(last)))
 elif (last == 0):
     print(s_num + "{} is {} and is 0".format(str(number), str(last)))
 elif (last < 6 and last != 0):
-    print(s_num + "{} is {}" + six_zero.format(str(number), str(last)))
+    print(s_num + "{} is -{}" + negative + "0".format(str(number), str(last)))
