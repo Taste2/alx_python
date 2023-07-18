8a53c49bb5f24ca8acdd3b66ca678bd44b732235
Taste2 <stawiah17@gmail.com>
2023-07-18 20:19:14 +0000

    task 6

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def validate_password(password):
+    if len(password) < 8:
+        return False
+    if " " in password:
+        return False
+    for upper in password:
+        if upper.isupper():
+            for lower in password:
+                if lower.islower():
+                    for numeric in password:
+                        if numeric.isnumeric():
+                            return True
+    return False
