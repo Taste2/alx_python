68918b209da4b31b6b792210ab5e9764a8b1d9c0
Taste2 <stawiah17@gmail.com>
2023-07-20 19:38:46 +0000

    changed logic of task 5

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def is_prime(number):
-    '''finds a prime number'''
-    if number % 2 == 0:
+    if number < 2:
         return False
-    else:
+    if number == 2:
         return True
-    
+    number_root = number ** 0.5
+    for i in range(2, int(number_root+1)):
+        if number % i == 0:
+            return False
+    return True
