44df957207e2c830cac7ecfacfb777be70e3e10f
Taste2 <stawiah17@gmail.com>
2023-07-24 19:39:34 +0000

    vars a and b removed from print statement

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -8,4 +8,4 @@ def safe_print_division(a, b):
         return result
     finally:
         result = a / b
-        print("Inside result: {} / {} = {}".format(a, b, result))
+        print("Inside result: {}".format(result))
