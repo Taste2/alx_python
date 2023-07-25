bef24d6cb0d4424980e171a7365e6fdf8a51f5c2
Taste2 <stawiah17@gmail.com>
2023-07-25 17:50:02 +0000

    task 1 first attempt

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv_len = len(sys.argv)
+    if argv_len == 0:
+        print("{} arguments.".format(argv_len))
+    elif argv_len == 1:
+        print("{} argument:\n".format(argv_len))
+        print("{}: {}".format(argv_len, sys.argv[0]))
+    else:
+        print("{} arguments:\n".format(argv_len))
+        for i in range(1, argv_len + 1):
+            print("{}: {}".format(i, sys.argv[i]))
