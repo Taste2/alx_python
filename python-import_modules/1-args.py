e4d2f7c856d585e471cb4f76d535598d4ddf195a
Taste2 <stawiah17@gmail.com>
2023-07-25 17:55:51 +0000

    new line removed

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if argv_len == 0:
         print("{} arguments.".format(argv_len))
     elif argv_len == 1:
-        print("{} argument:\n".format(argv_len))
+        print("{} argument:".format(argv_len))
         print("{}: {}".format(argv_len, sys.argv[0]))
     else:
-        print("{} arguments:\n".format(argv_len))
+        print("{} arguments:".format(argv_len))
         for i in range(1, argv_len + 1):
             print("{}: {}".format(i, sys.argv[i]))
