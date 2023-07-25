adfddbcb2856efd095115d0215ba23e1fa848571
Taste2 <stawiah17@gmail.com>
2023-07-25 23:43:29 +0000

    start with empty string and add

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = []
+    new_string = ""
     for char in my_string:
         if char == 'c' or char == 'C':
             del char
         else:
-            print(char, end=" ")
+            new_string = new_string + char
+    return new_string
