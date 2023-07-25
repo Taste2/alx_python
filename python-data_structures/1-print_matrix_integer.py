ceafc831b54b1a6b203471bf526c584001290719
Taste2 <stawiah17@gmail.com>
2023-07-25 23:59:33 +0000

    task 1 first attmpt

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    a = matrix[0]
+    b = matrix[1]
+    c = matrix[2]
+    print("{}".format(a))
+    print("{}".format(b))
+    print("{}".format(c))
