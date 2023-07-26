e24d98ae3133026858c2b14993323608ee5c6f42
Taste2 <stawiah17@gmail.com>
2023-07-26 11:07:25 +0000

    check if matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == 0:
-        print("\n")
-    else:
-        for mat in matrix:
-            for m in mat:
-                print("{:d}".format(m), end=" " if m != mat[-1] else '\n')
+    if not matrix:
+        print()
+        return
+    for mat in matrix:
+        for m in mat:
+            print("{:d}".format(m), end=" " if m != mat[-1] else '\n')
