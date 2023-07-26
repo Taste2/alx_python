db7591437cbdb0f175fa70535d423e5daa857304
Taste2 <stawiah17@gmail.com>
2023-07-26 10:38:16 +0000

    fixing empty matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for mat in matrix:
-        if mat == None:
-            print("\n")
-        for m in mat:
-            print("{:d}".format(m), end=" " if m != mat[-1] else '\n')
+    if matrix == 0:
+        print("\n")
+    else:
+        for mat in matrix:
+            for m in mat:
+                print("{:d}".format(m), end=" " if m != mat[-1] else '\n')
