17630efcf0b438436fd2188817dedf6d617268ad
Taste2 <stawiah17@gmail.com>
2023-07-26 00:23:04 +0000

    added for  loop

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,6 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     a = matrix[0]
     b = matrix[1]
     c = matrix[2]
-    print("{}".format(a))
-    print("{}".format(b))
-    print("{}".format(c))
+    for i in a:
+        print("{:d}".format(i), end=" " if i == a[0] or i == a[1] else '\n')
+    for j in b:
+        print("{:d}".format(j), end=" " if j == b[0] or j == b[1] else '\n')
+    for k in c:
+        print("{:d}".format(k), end=" ")
