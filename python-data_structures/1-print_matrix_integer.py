72bce494caf328959b92efcc816ed99c3419bc66
Taste2 <stawiah17@gmail.com>
2023-07-26 11:17:24 +0000

    removed return

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
-        return
+
     for mat in matrix:
         for m in mat:
             print("{:d}".format(m), end=" " if m != mat[-1] else '\n')
