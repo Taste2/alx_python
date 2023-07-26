b3bf1e9eb92b9c3b1a31ffae61decf28aa364489
Taste2 <stawiah17@gmail.com>
2023-07-26 09:44:48 +0000

    changed logic to cater for all cases

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    a = matrix[0]
-    b = matrix[1]
-    c = matrix[2]
-    for i in a:
-        print("{:d}".format(i), end=" " if i == a[0] or i == a[1] else '\n')
-    for j in b:
-        print("{:d}".format(j), end=" " if j == b[0] or j == b[1] else '\n')
-    for k in c:
-        print("{:d}".format(k), end=" ")
+    for mat in matrix:
+        for m in mat:
+            print("{:d}".format(m), end=" " if m != mat[-1] else '\n')
