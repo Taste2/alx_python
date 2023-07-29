1ded1c0471c60e8c8329912e2908a19efe2625a8
Taste2 <stawiah17@gmail.com>
2023-07-29 13:39:35 +0000

    changed logic

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for k, v in a_dictionary.items():
+    for k in a_dictionary():
         if k != key:
             a_dictionary[key] = value
         elif k == key:
