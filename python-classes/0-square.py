9d3d04b4d195a86a143903b9aaa3d69814a24c7a
Taste2 <stawiah17@gmail.com>
2023-08-01 12:12:39 +0000

    improving on docstrings

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,26 @@
-"""This module is a definition of a class Square"""
+"""
+This module is a definition of a class Square that represents a square.
+
+Attributes:
+    size(int): The length of each side of the square.
+
+Methods:
+    __init__(self, size): Initializes an instance or object of the class
+"""
 class Square():
-    """This class defines a square with its sizes"""
+    """
+    A class that represents a square
+    
+    Attributes:
+        size(int): The size of the square
+
+    Methods:
+        __init__(self, size): Initializes a Square object with the given size.
+    """
 
     def __init__(self, size):
-        """Initialize attribute"""
-        self._size = size
+        """Initialize a square instance
+            Args:
+                size(int): the size of the square
+                """
+        self.__size = size
