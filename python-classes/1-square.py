a310dbe34eb642fe7f74950bb23ed0aec7391c1e
Taste2 <stawiah17@gmail.com>
2023-08-01 20:03:42 +0000

    raising the right exception

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -21,24 +21,11 @@ class Square():
     def __init__(self, size=0):
         """Initialize a square instance
             Args:
-                size(int): the size of the square
+                size(int): the size of the square initialized to 0
                 """
         self.__size = size
 
-    @property
-    def sq_size(self):
-        """
-        a getter to access the attribute __size
 
-        Args:
-            self: refers to the instance itself
-
-        Return:
-            returns teh size attribute
-        """
-        return self.__size
-    
-    @sq_size.setter
     def sq_size(self, size):
         """
         A setter to set the size of attribute to an integer
@@ -46,22 +33,11 @@ class Square():
         Args:
             size: The size of the square
         """
-        if size <= 0 or size >= 0:
+        if size == int(size):
             self.__size = size
         else:
-            raise TypeError
-        print("size must be an integer")
-    
-    @sq_size.setter
-    def sq_size(self, size):
-        """
-        A setter to set the size of attribute to an integer 0 and positive
-
-        Args:
-            size: The size of the square
-        """
+            raise TypeError("size must be an integer")
         if size  >= 0:
             self.__size = size
         else:
-            raise ValueError
-        print("size must be >= 0")
+            raise ValueError("size must be >= 0")
