917af4f8a75c97e5c8c18845dcf51592ef88d31f
Taste2 <stawiah17@gmail.com>
2023-08-01 21:39:08 +0000

    task 2 first attempt

---
+++ python-classes/2-square.py
@@ -0,0 +1,40 @@
+"""
+This module is a definition of a class Square that represents a square.
+
+Attributes:
+    size(int): The length of each side of the square.
+
+Methods:
+    __init__(self, size): Initializes an instance or object of the class
+
+    area: Returns the area of the square
+"""
+class Square():
+    """
+    A class that represents a square
+    
+    Attributes:
+        size(int): The size of the square
+
+    Methods:
+        __init__(self, size): Initializes a Square object with the given size.
+        area: Returns the area of the square
+    """
+
+    def __init__(self, size=0):
+        """Initialize a square instance
+            Args:
+                size(int): the size of the square initialized to 0
+                """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size  < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Method that computes area of the square
+        """
+        sq_area = self.__size ** 2
+        return sq_area
