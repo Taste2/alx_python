04c7c2142fb659cf17237f0ac07b86494a131ce9
Taste2 <stawiah17@gmail.com>
2023-08-02 18:56:19 +0000

    -> None

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,12 +7,12 @@ class BaseGeometry():
     """
     This class models an empty class
     """
-    def __dir__(cls):
+    def __dir__(cls) -> None:
         """
         control access to some inherited attributes
         """
-        n_attributes = []
         attributes = super().__dir__()
+        n_attributes = []
         for attr in attributes:
             if attr != '__init_subclass__':
                 n_attributes.append(attr)
