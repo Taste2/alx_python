8ab13faa3a1995e81c4b7dc0a1e148e4d589b111
Taste2 <stawiah17@gmail.com>
2023-08-02 11:55:46 +0000

    from issubclass to isinstance

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,19 +1,19 @@
 """
-This module checks if an instance is directly or indirectly inherits a specific class
+This module checks if an instance is directly or indirectly a n instance of specific class
 
 """
 def inherits_from(obj, a_class):
-    """AI is creating summary for inherits_from
-
+    """
+    check if obj is an instance of class
     Args:
         obj ([type]): object
         a_class ([type]): a class
 
     Returns:
-        True: obj is a subclass of a_class
-        False: obj not a sub class of a_class
+        True: obj is an instance of a_class
+        False: obj not an instance of a_class
     """
-    if issubclass(obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
