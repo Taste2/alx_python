eddfc036190221271ef0ab126de916d4e79a2fb2
Taste2 <stawiah17@gmail.com>
2023-08-02 21:42:13 +0000

    fixing condition

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -17,7 +17,7 @@ def inherits_from(obj, a_class):
         False: obj not an instance of a_class
     """
 
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
