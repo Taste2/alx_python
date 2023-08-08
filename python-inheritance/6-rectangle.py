5c17b0cb45bf0dac2d72b5757e412b8a4d231ddd
Taste2 <stawiah17@gmail.com>
2023-08-08 13:51:08 +0000

    import base geo

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,37 +1,9 @@
-"""
-This module is an empty class 
-"""
 
+"""
+Module containing the Rectangle class.
+"""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-class BaseGeometry():
-    """
-    This class models an empty class
-    """
-    def __dir__(cls) -> None:
-        """
-        control access to some inherited attributes
-        """
-        attributes = super().__dir__()
-        n_attributes = []
-        for attr in attributes:
-            if attr != '__init_subclass__':
-                n_attributes.append(attr)
-        return n_attributes
-    
-    def area(self):
-        """a method to raise an exception with a message"""
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        """
-        A method that validates value
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        
-    
 class Rectangle(BaseGeometry):
     """
     Models a rectangle. A derived class of BaseGeometgry
@@ -47,12 +19,11 @@ class Rectangle(BaseGeometry):
                 n_attributes.append(attr)
         return n_attributes
     
-    def __init__(self, width, height) -> None:
+    def __init__(self, width, height):
         """
         Call attriutes of parent.
         validates attributes for positive integer
         """
-        super().__init__()
         self.__width = width
         self.__height = height
         self.__width.integer_validator("width", width)
