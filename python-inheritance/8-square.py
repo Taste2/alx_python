5ef7e3642539dd375201fd8a84fcdb676da45ff5
Taste2 <stawiah17@gmail.com>
2023-08-08 22:53:38 +0000

    pep fixed

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -13,7 +13,7 @@ class Square(Rectangle):
     """
     def __init__(self, size):
         """
-        instantiate attributes to an object 
+        instantiate attributes to an object
         """
         self.integer_validator("size", size)
         self.__size = size
@@ -25,7 +25,7 @@ class Square(Rectangle):
             str: String representation of the Square instance.
         """
         return "[Rectangle] {}/{}".format(self.__size, self.__size)
-    
+
     def __dir__(cls):
         """
         Returns:
