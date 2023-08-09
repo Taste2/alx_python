ab5307f84798f5d7c2e3c759a957d6972c43c523
Taste2 <stawiah17@gmail.com>
2023-08-09 01:05:04 +0000

    Task 0 first attempt

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+"""
+This Class is the base for subsequent projects
+"""
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            __nb_objects =+ 1
+            self.id = __nb_objects
