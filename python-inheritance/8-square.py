1edb8ff06d1465734c877707fca7ef0303a38947
Taste2 <stawiah17@gmail.com>
2023-08-09 14:38:03 +0000

    pep implementation

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -2,6 +2,8 @@
 """
 Module containing the Rectangle class.
 """
+
+
 class OverrideMetaClass(type):
     """def __new__(cls, name, bases, attrs):
         # Customize the class creation process here
