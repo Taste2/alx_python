bf59721f81b364691f43e8d5444dc8e2711c1cd6
Taste2 <stawiah17@gmail.com>
2023-08-09 01:57:02 +0000

    access class atrr and change operator fr increnent

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,7 +7,6 @@ class Base:
     Base class for subsequent tasks
     """
     __nb_objects = 0
-
     def __init__(self, id=None):
         """
         Class constructor. Pass attributes to objects
@@ -15,5 +14,5 @@ class Base:
         if id != None:
             self.id = id
         else:
-            __nb_objects =+ 1
-            self.id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
