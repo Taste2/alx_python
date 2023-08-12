931902eb24c704d53b0815ced51d506bcda6a87f
Taste2 <stawiah17@gmail.com>
2023-08-12 02:25:07 +0000

    task5 attempt

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,13 @@
+"""
+ Python script that takes your GitHub credentials
+   (username and password) and uses the GitHub API to display your id
+"""
+import sys 
+import requests
+
+url = 'https://api.github.com/user'
+username = sys.argv[1]
+password = sys.argv[2]
+
+response = requests.get(url)
+print(response.json()['id'])
