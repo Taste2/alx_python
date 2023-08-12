55f16d2e28b212cb6f20f8f2bf4f07872a1f4bc2
Taste2 <stawiah17@gmail.com>
2023-08-12 00:29:41 +0000

    Task4 first attempt

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,22 @@
+"""
+Python script that takes in a letter and sends a POST request to
+ http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import sys
+import requests
+
+q = sys.argv[1]
+if q is False:
+    q = ""
+
+parameter = {'q':q}
+url = 'http://0.0.0.0:5000/search_user'
+response = requests.post(url, json=parameter)
+
+if response.json():
+    for key, value in response.json().items():
+        print("[{}] {}".format(key, value))
+elif response.json() == {}:
+    print("No result")
+else:
+    print("Not a valid JSON")
