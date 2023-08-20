23e1af2f3a6d7d9194c8fb2ec0ac9cc52afcffde
Taste2 <stawiah17@gmail.com>
2023-08-20 15:14:36 +0000

    no execution when imported

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 '''
 This program lists all states from the database hbtn_0e_0_usa
 '''
+if __name__ == "__main__":
+    import MySQLdb
 
-import MySQLdb
+    # Connect to the database
+    connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_0')
 
-#Connect to the database
-connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_0')
+    # a cursor to manipulate the database
+    db_cur = connector.cursor()
 
-# a cursor to manipulate the database
-db_cur = connector.cursor()
-
-db_cur.execute("USE test_0")
-db_cur.execute("SELECT name FROM states ORDER BY states.id")
+    db_cur.execute("USE test_0")
+    db_cur.execute("SELECT name FROM states ORDER BY states.id")
