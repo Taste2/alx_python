6747231e36e78a62cf8fbc2fcd7049f618d8c19f
Taste2 <stawiah17@gmail.com>
2023-08-20 17:35:35 +0000

    added , in connector

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ This program lists all states from the database hbtn_0e_0_usa
 if __name__ == "__main__":
     import MySQLdb
     # Connect to the database
-    connector = MySQLdb.connect(host='localhost' user='root', passwd='St10285515', db='test_0')
+    connector = MySQLdb.connect(host='localhost', user='root', passwd='St10285515', db='test_0')
 
     # a cursor to manipulate the database
     db_cur = connector.cursor()
