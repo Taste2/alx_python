2b0c58ba9f86b780b6b9581cc8846001eeefdc3c
Taste2 <stawiah17@gmail.com>
2023-08-20 15:57:52 +0000

    import below if statement

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,8 @@
 '''
 This program lists all states from the database hbtn_0e_0_usa
 '''
-import MySQLdb
 if __name__ == "__main__":
-
+    import MySQLdb
     # Connect to the database
     connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_0')
 
@@ -12,4 +11,3 @@ if __name__ == "__main__":
 
     db_cur.execute("USE test_0")
     db_cur.execute("SELECT * FROM states ORDER BY states.id")
-    
