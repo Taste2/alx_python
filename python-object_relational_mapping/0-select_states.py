d32efb15a184ecd19b7230bd1efe4d79516007a5
Taste2 <stawiah17@gmail.com>
2023-08-20 18:29:20 +0000

    revert to where coded worked: for loop

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,11 @@ This program lists all states from the database hbtn_0e_0_usa
 if __name__ == "__main__":
     import MySQLdb
     # Connect to the database
-    connector = MySQLdb.connect(user='stephen', passwd='password', db='test_0')
+    connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_0')
 
     # a cursor to manipulate the database
     db_cur = connector.cursor()
 
-    db_cur.execute("CREATE USER IF NOT EXISTS 'stephen'@'localhost'")
-    db_cur.execute("SET PASSWORD FOR 'stephen'@'localhost' = 'password")
-    db_cur.execute("GRANT ALL PRIVILEGES ON *.* TO 'stephen'@'localhost'")
     db_cur.execute("USE test_0")
     db_cur.execute("SELECT * FROM states")
     states_data = db_cur.fetchall()
