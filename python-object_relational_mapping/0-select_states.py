0c88aafebf31c8074a75e995ba8d948741b60225
Taste2 <stawiah17@gmail.com>
2023-08-20 15:20:12 +0000

    select all rows

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,11 @@ import MySQLdb
 if __name__ == "__main__":
 
     # Connect to the database
-    connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_0')
+    connector = MySQLdb.connect(user='stephen', passwd='password', db='test_0')
 
     # a cursor to manipulate the database
     db_cur = connector.cursor()
 
     db_cur.execute("USE test_0")
-    db_cur.execute("SELECT name FROM states ORDER BY states.id")
+    db_cur.execute("SELECT * FROM states ORDER BY states.id")
+    
