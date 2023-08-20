5203cd72f0e9cbbb5cb0d3e1a91af012ccde4679
Taste2 <stawiah17@gmail.com>
2023-08-20 15:08:08 +0000

    changed database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,10 @@ This program lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 
 #Connect to the database
-connector = MySQLdb.connect(user='root', passwd='St10285515', db='hbtn_0e_0_usa')
+connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_0')
 
 # a cursor to manipulate the database
 db_cur = connector.cursor()
 
-db_cur.execute("USE hbtn_0e_usa")
+db_cur.execute("USE test_0")
 db_cur.execute("SELECT name FROM states ORDER BY states.id")
