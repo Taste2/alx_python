da974d2b1717d38eab357c239cdccead9bdd08e7
Taste2 <stawiah17@gmail.com>
2023-08-21 15:34:50 +0000

    task2 first attempt

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+'''
+Write a script that takes in an argument and displays all 
+values in the states table of hbtn_0e_0_usa 
+where name matches the argument.
+'''
+if __name__ == "__main__":
+    import MySQLdb
+
+    searched_state = input("Enter any State: ")
+    # Connect to the database
+    connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_2')
+
+    # a cursor to manipulate the database
+    db_cur = connector.cursor()
+
+    db_cur.execute("USE test_2")
+    query = "SELECT * FROM states \
+                   WHERE name COLLATE utf8mb4_bin LIKE '{}%'".format(searched_state)
+    db_cur.execute(query)
+    states_data = db_cur.fetchall()
+
+    for data in states_data:
+        print(data)
