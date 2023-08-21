c2a2695c8384d3d85beaac700728986dd1fd39dd
Taste2 <stawiah17@gmail.com>
2023-08-21 17:27:40 +0000

    task6 first attempt

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+'''
+a python file that contains the class definition of a
+State and an instance Base = declarative_base():
+'''
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create engine
+DATABASE_URI = "mysql://root:St10285515"
+engine = create_engine(DATABASE_URI)
+
+# Base class
+Base = declarative_base
+
+# A class to create the tables
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+
+Base.metadata.create_all(bind=engine)
