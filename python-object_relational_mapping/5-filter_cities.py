79a6d31832616f82795009b85cc0537f9d473d29
Taste2 <stawiah17@gmail.com>
2023-08-22 21:37:18 +0000

    rem close()

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,13 +6,12 @@ using the database hbtn_0e_4_usa
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    
 
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_searched = sys.argv[4]
-    
+
     # Connect to the database
     connector = MySQLdb.connect(user=user, passwd=password, db=database)
 
@@ -28,6 +27,3 @@ if __name__ == "__main__":
 
     cities = ', '.join(data[0] for data in states_data)
     print(cities)
-
-    connector.close()
-    db_cur.close()
