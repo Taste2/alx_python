e8438394f76df84d3faadf44a0b422e808e77ab6
Taste2 <stawiah17@gmail.com>
2023-08-22 06:28:13 +0000

    pass arguments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,14 @@ This program lists all states from the database hbtn_0e_0_usa
 '''
 if __name__ == "__main__":
     import MySQLdb
+    import sys
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     # Connect to the database
-    connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_0')
+    connector = MySQLdb.connect(user=user, passwd=password, db=database)
 
     # a cursor to manipulate the database
     db_cur = connector.cursor()
