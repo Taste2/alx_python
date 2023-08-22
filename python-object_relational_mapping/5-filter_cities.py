b3f7ba1d61a90a47edc3bc9cb966f94390a90117
Taste2 <stawiah17@gmail.com>
2023-08-22 16:21:31 +0000

    remove comma after name

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     # a cursor to manipulate the database
     db_cur = connector.cursor()
-    db_cur.execute("""SELECT id, name,
+    db_cur.execute("""SELECT id, name
                    FROM cities
                    WHERE states_id =
                     (SELECT id FROM states
