25b4d4dcda2ee1ddbad6608898ab5139e533fa4f
Taste2 <stawiah17@gmail.com>
2023-08-22 08:08:40 +0000

    removed collate

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,16 +7,18 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    # Connect to the database
-    connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_3')
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
+    # Connect to the database
+    connector = MySQLdb.connect(user=user, passwd=password, db=database)
     # a cursor to manipulate the database
     db_cur = connector.cursor()
 
-    db_cur.execute("USE test_3")
     state_search = sys.argv[4]
     query = "SELECT * FROM states \
-            WHERE name COLLATE utf8mb4_bin LIKE %s"
+            WHERE name LIKE %s"
     db_cur.execute(query, (state_search + '%'),)
     states_data = db_cur.fetchall()
 
