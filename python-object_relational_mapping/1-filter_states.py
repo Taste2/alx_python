d354e74c592b9642305c6a416ad0a2f7a6e98706
Taste2 <stawiah17@gmail.com>
2023-08-22 06:30:52 +0000

    pass arguments

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,13 +4,18 @@ name starting with N (upper N) from the database hbtn_0e_0_usa:
 '''
 if __name__ == "__main__":
     import MySQLdb
+    import sys
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     # Connect to the database
-    connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_1')
+    connector = MySQLdb.connect(user=user, passwd=password, db=database)
 
     # a cursor to manipulate the database
     db_cur = connector.cursor()
 
-    db_cur.execute("USE test_1")
     query = "SELECT * FROM states \
                    WHERE name COLLATE utf8mb4_bin LIKE 'N%'"
     db_cur.execute(query)
