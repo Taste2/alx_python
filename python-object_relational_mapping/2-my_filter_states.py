22721484b08c0a6dafadafb61b11dbd97f4b54cc
Taste2 <stawiah17@gmail.com>
2023-08-22 06:33:08 +0000

    pass arguments

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,12 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     # Connect to the database
-    connector = MySQLdb.connect(user='root', passwd='St10285515', db='test_2')
+    connector = MySQLdb.connect(user=user, passwd=password, db=database)
 
     # a cursor to manipulate the database
     db_cur = connector.cursor()
