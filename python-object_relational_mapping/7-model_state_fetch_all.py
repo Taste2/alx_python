8f7c23a76a7d593c8ef461a55ce2a19f3f9e46a1
Taste2 <stawiah17@gmail.com>
2023-08-24 21:45:22 +0000

    print element in for loop

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    for name in session.query(State).all():
-        print(f"{State.id}: {State.name}")
+    for state in session.query(State).order_by(State.id).all():
+        print(f"{state.id}: {state.name}")
