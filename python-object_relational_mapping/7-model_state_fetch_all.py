a25664898949227ffa099863e50a71d152860d26
Taste2 <stawiah17@gmail.com>
2023-08-24 15:29:19 +0000

    restructure

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,5 +4,5 @@ A script that lists all State objects from the database hbtn_0e_6_usa
 from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
 
-for id, name in session.query(State.name):
+for id, name in session.query(State.id, State.name):
     print(f"{id}: {name}")
