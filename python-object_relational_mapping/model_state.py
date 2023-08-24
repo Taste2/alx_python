3958b9fcf35441b58cb4a61cb57c381fb19e4175
Taste2 <stawiah17@gmail.com>
2023-08-24 10:43:48 +0000

    fixing class documentation error

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,37 +1,7 @@
-'''
-This module is a python file that contains the class definition of a
-State and an instance Base = declarative_base():
-
-class:
-State: Model of the table states which inherits from Base
- attr:
-    id: unigue id of each state
-    name: names of the states
-'''
-
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import MySQLdb
 
-username = 'root'
-password = 'St10285515'
-database = 'test_6'
-# Create connection URL
-connector = "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, database)
-
-# Create an SQLAlchemy engine
-engine = create_engine(connector)
-
-# Establish a connection to the database
-connection = engine.connect()
-
-# Base class
 Base = declarative_base()
-'''
-A base class that serves as the foundation for defining
-database models (tables) using the
-Object-Relational Mapping (ORM) approach.
-'''
 
 # A class to create the tables
 class State(Base):
@@ -48,5 +18,7 @@ class State(Base):
     id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
 
+    def __init__(self, name):
+        self.name = name
 
 Base.metadata.create_all(bind=engine)
