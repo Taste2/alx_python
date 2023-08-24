e37469e15a6078c0d74275f2278d69bcdd709560
Taste2 <stawiah17@gmail.com>
2023-08-24 22:03:59 +0000

    PEP8 validation

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import sys
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
     Base.metadata.create_all(engine)
 
