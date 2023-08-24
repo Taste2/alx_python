76de31cee7a18221c18b65a7c59a8e2ee7e43f80
Taste2 <stawiah17@gmail.com>
2023-08-24 22:35:35 +0000

    task9 first attempt

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 '''
-A script that lists all State objects from the database hbtn_0e_6_usa
+a script that prints the first State object from the database 
 '''
 if __name__ == "__main__":
     from model_state import State, Base
