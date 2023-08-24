7f23d5a2242224487708bd8e85011f6169d6b3bf
Taste2 <stawiah17@gmail.com>
2023-08-24 22:21:52 +0000

    removed for loop

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).first():
-        if state:
-            print("{}: {}".format(state.id, state.name))
-        else:
-            print("", end='\n')
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("", end='\n')
 
     session.close()
