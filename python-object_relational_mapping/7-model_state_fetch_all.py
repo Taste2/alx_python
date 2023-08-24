d013eeacbfcbe93ad234d4e67a298405d3189f8b
Taste2 <stawiah17@gmail.com>
2023-08-24 21:54:37 +0000

    used str.format()

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
+
     for state in session.query(State).order_by(State.id).all():
-        print(f"{state.id}: {state.name}")
-    
+        print("{}: {}".format(state.id, state.name))
+
     session.close()
