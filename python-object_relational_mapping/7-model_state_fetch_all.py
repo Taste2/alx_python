03461efd4138c79f8e19ab74b9c409d4de0b0ea9
Taste2 <stawiah17@gmail.com>
2023-08-24 22:10:22 +0000

    PEP8 validation

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     from sqlalchemy import create_engine
     import sys
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-    .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    path = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(path)
 
     Base.metadata.create_all(engine)
 
