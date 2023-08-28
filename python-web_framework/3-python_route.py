2f672440f2948a362a91e7d28626850314f1872e
Taste2 <stawiah17@gmail.com>
2023-08-28 00:31:57 +0000

    removed escape

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -57,7 +57,7 @@ def python_(text):
     return: a text
     '''
     text = text.replace('_', " ")
-    return "Python {}".format(escape(text))
+    return "Python {}".format(text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
